768693dc05412c21589f3e9e031f422a677e15ae
po1o <m.gromov.nov@gmail.com>
2020-12-30 01:26:38 +0300

    Add demo and prod environments

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ import telebot
 from telebot import types
 import json
 import random
-import config
+import demo_config as config
 
-telegram_token = config.TOKEN
 
-bot = telebot.TeleBot(telegram_token)
+token = config.TOKEN
+welcome_text = config.WELCOME_TEXT
+images_folder = config.IMAGES_FOLDER
+photos_folder = config.PHOTOS_FOLDER
+persons_list = config.PERSONS_LIST
+
+bot = telebot.TeleBot(token)
 
 
 def send_question(message):
-    persons = get_json_from_file('persons.json')
+    persons = get_json_from_file(f'{persons_list}.json')
     person = random.choice(persons)
     photo_src = get_photo_src(person['id'])
     answer_options = get_answer_options(person['name'], person['gender'])
     markup = get_markup(answer_options)
     with open(photo_src, 'rb') as photo:
-        question_msg = bot.send_photo(message.chat.id, photo,
-                                      reply_markup=markup)
+        question_msg = bot.send_photo(message.chat.id, photo, reply_markup=markup)
     bot.register_next_step_handler(question_msg, check_answer, person)
 
 
@@ -28,13 +32,13 @@ def get_json_from_file(file_name):
 
 
 def get_photo_src(person_id):
-    photo_src = f'photos/{person_id}.jpg'
+    photo_src = f'{photos_folder}/{person_id}.jpg'
     return photo_src
 
 
 def get_answer_options(person_name, person_gender):
     answer_options = [person_name]
-    persons = get_json_from_file('persons.json')
+    persons = get_json_from_file(f'{persons_list}.json')
     random_persons_by_gender = [
         person for person in persons if person['gender'] == person_gender]
     while len(answer_options) < 4:
@@ -47,25 +51,25 @@ def get_answer_options(person_name, person_gender):
 
 def get_markup(answer_options):
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    markup.add(*(types.KeyboardButton(answer_option)
-                 for answer_option in answer_options))
+    markup.add(*(types.KeyboardButton(answer_option) for answer_option in answer_options))
     return markup
 
 
 def check_answer(message, person):
     if message.text == person['name']:
-        answer_msg = bot.send_message(message.chat.id, u'\U00002705' + ' Верно!')
+        answer_msg = bot.send_message(
+            message.chat.id, u'\U00002705' + ' Верно!')
     else:
-        answer_msg = bot.send_message(message.chat.id, u'\U0000274C' + ' Ошибка!')
+        answer_msg = bot.send_message(
+            message.chat.id, u'\U0000274C' + ' Ошибка!')
     send_question(message)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    welcome_text = config.WELCOME_MESSAGE
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton('Го!'))
-    with open('images/welcome_image.png', 'rb') as photo:
+    with open(f'{images_folder}/welcome_image.png', 'rb') as photo:
         welcome_msg = bot.send_photo(message.chat.id, photo, welcome_text, reply_markup=markup)
     bot.register_next_step_handler(welcome_msg, send_question)
 
--- config_example.py
+++ config_example.py
@@ -1,3 +1,6 @@
 # rename this file to config.py
 TOKEN = 'YOUR_TOKEN'
 WELCOME_MESSAGE = 'Hello, World!'
+IMAGES_FOLDER = 'demo_images'
+PHOTOS_FOLDER = 'demo_photos'
+PERSONS_LIST = 'demo_persons'
